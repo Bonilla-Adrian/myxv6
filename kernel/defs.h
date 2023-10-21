3063bfd02c07427894a1aeec9a11ce4771833eec
Adrian Bonilla <adrian.bonilla1993@gmail.com>
2023-10-20 17:14:01 -0700

    Added functions to defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -105,6 +105,8 @@ int             either_copyout(int user_dst, uint64 dst, void *src, uint64 len);
 int             either_copyin(void *dst, int user_src, uint64 src, uint64 len);
 void            procdump(void);
 int             procinfo(uint64);
+int		getpriority(int pid);
+int		setpriority(int pid, int priority);
 
 // swtch.S
 void            swtch(struct context*, struct context*);
